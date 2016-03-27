16a73917f737491024744f95ab538a84d314eeb2
redpeaks33 <redpeaks33@gmail.com>
2016-03-27 17:58:18 +0900

    AngularJS Routing
    AngularJS Routing

---
+++ Routing/js/controller.js
@@ -0,0 +1,13 @@
+﻿var main = angular.module("app", ['ui.router']);
+
+main.controller('MyController', ['$scope', '$http', function ($scope, $http) {
+    $scope.Message = 'Click Button';
+
+    $scope.GetStart = function () {
+        $http.get("api/Contents/1").success(function (data, status, headers, config) {
+            $scope.Message += ':' + data;
+        }).error(function (data, status, headers, config) {
+            alert('error');
+        });
+    };
+}]);
